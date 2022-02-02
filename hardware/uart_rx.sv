3380f450717e7db5512333a03a80341502d57b9a
Alexander Schramm <alexander.schramm96@gmail.com>
2022-02-02 20:44:58 +0100

    Added UART receiver module

--- hardware/uart_rx.sv
+++ hardware/uart_rx.sv
@@ -1,39 +1,115 @@
 module uart_rx(
-	input clk, rst, rx_enable, rx_line,
-	output [7:0] data,
-	output data_ready, rx_buf_full
+	input clk, rst, rx,
+	input [15:0] bit_duration, start_bit_duration,
+	input [1:0] stopbits,
+	output reg [7:0] data,
+	output reg data_ready
 );
 
-reg rx_tmp, rx;
+reg [3:0] rx_shift_reg;
+reg [16:0] ctr; // larger than bit_duration to prevent overflows during internal calculations
+reg [4:0] bit_ctr;
+reg [1:0] cur_state;
 
-// TODO: sample rx line and apply low pass filter (n consecutive, identical samples are considered a valid state change )
+
+
+
+// States for uart statemachine
+parameter STATE_IDLE = 2'b00;
+parameter STATE_RECEIVE_START = 2'b01;
+parameter STATE_READ_DATA = 2'b10;
+parameter STATE_STOPBITS = 2'b11;
 
 always @ (posedge clk) begin
-	
-	// Double flopping rx to prevent metastability
-	rx_tmp <= rx_line;
-	rx <= rx_tmp;
 
-	if (rst | !rx_enable) begin
+	if(rst) begin
+		// set internal registers to 0
+		ctr <= 0;
+		rx_shift_reg <= 0;
+		bit_ctr <= 0;
+
+		// set outputs to 0
 		data <= 0;
 		data_ready <= 0;
 	end else begin
+		
+		// Shift register of past rx values for filtering etc
+		rx_shift_reg <= {rx, rx_shift_reg[3:1]};
+		ctr = ctr + 1;
+	
 		// rx state machine here
-		case(cur_state):
+		case(cur_state)
 			default: cur_state <= STATE_IDLE;
+			STATE_IDLE: begin
+				// check for falling edge with some basic filtering
+				if((rx_shift_reg == 4'b0001) || (rx_shift_reg == 4'b0010)) begin
+					cur_state <= STATE_RECEIVE_START;
+				end
+
+				ctr <= 3;
+				data_ready <= 0;
+				bit_ctr <= 0;
+				
+				//if(!data_ready) begin
+					data <= 0;
+				//end
+			end
 			STATE_RECEIVE_START: begin
-				if(rx == 0) begin // TODO: instead of just checking rx== 0, it should be checked if rx was 0 for more than START_BIT_MIN_FILTER cycles
-					// start bit received
-					// wait until counter reaches START_BIT_PERIOD, then set state to STATE_START
+				if(rx != 0 && ctr < start_bit_duration) begin 
+					// rx went back high again, this wasn't a valid stop bit.
+					// reset to idle state
+					cur_state <= STATE_IDLE;
+					ctr <= 0;
+				end
+
+				// Check if start bit is over, if yes, move to next state
+				if(ctr == bit_duration) begin
+					ctr <= 0;
+					cur_state <= STATE_READ_DATA;
 				end
 			end
-			STATE_START: begin
+			STATE_READ_DATA: begin
+				if(ctr == bit_duration >> 1) begin
+					// we're in the middle of the bit, this is the point where we want to read the bit
+
+					// increment the bit counter
+					bit_ctr <= bit_ctr + 1;
+
+					// shift in the bit that was read
+					data <= {rx, data[7:1]}; 
+				end  
+
+				if(ctr == bit_duration) begin
+					// at this point the current bit was read, we can either read the next data bit or the stop bit(s)
+
+					// we have read all 8 databits, read stop bit next
+					if(bit_ctr == 8) begin
+						cur_state <= STATE_STOPBITS;
+						ctr <= 0;
+					end else begin
+						ctr <= 0;
+					end
+				end
 				// rx was low for long enough to count as uart start bit, now we wait for the START_BIT_LENGTH + half baud length, then go to STATE_RECEIVE_DATA
 			end
-			STATE_RECEIVE_DATA: begin
-				// wait for half a bit period (half baud period), then sample first bit, then wait 1 period, sample second bit, ...
+			STATE_STOPBITS: begin
+				// Count to stop bit duration, then reset state to idle
+				if((stopbits == 2'b00) && (ctr == bit_duration >> 1)) begin // 0.5 stop bits
+					data_ready <= 1;
+					cur_state <= STATE_IDLE;
+				end else if ((stopbits == 2'b01) && (ctr == bit_duration)) begin // 1 stop bit
+					data_ready <= 1;
+					cur_state <= STATE_IDLE;
+				end else if ((stopbits == 2'b10) && (ctr == {1'b0,bit_duration[15:0]} + bit_duration >> 1)) begin
+					// this condition adds half of bit_duration to bit_duration in a 17-bit wide register to get 1.5x bit_duration
+					data_ready <= 1;
+					cur_state <= STATE_IDLE;
+				end else if(ctr[15:1] == bit_duration) begin // functionally equivalent to left shift of bit_duration
+					data_ready <= 1;
+					cur_state <= STATE_IDLE;
+				end
 			end
-					
+		endcase
 	end
 end
    
--- hardware/uart_rx_tb.sv
+++ hardware/uart_rx_tb.sv
@@ -1,4 +1,5 @@
 `timescale 1ns/1ps
+`include "./hardware/uart_rx.sv"
 
 module uart_rx_tb;
 
@@ -15,6 +16,20 @@ int unsigned nanosecondsPerSec = 1000000000;
 real delay_time_f = real'(nanosecondsPerSec) / real'(baudrate);
 int unsigned delay_time = $ceil(delay_time_f);
 
+
+int unsigned test_clk_period = 10; // ns/clk
+int unsigned bit_duration_ns = delay_time; // ns/bit
+int unsigned bit_duration_ctr = bit_duration_ns / test_clk_period; // clk/bit
+
+reg clk, rst;
+wire data_ready;
+wire [15:0] bit_duration;
+wire [1:0] stopbits;
+wire [7:0] data;
+
+
+uart_rx my_uart(.clk(clk), .rst(rst), .rx(uart_tx_test), .data_ready(data_ready), .bit_duration(bit_duration_ctr[15:0]), .start_bit_duration(bit_duration_ctr[15:0]), .stopbits(stopbits), .data(data));
+
 initial begin
 	// Dump uart transmission to file
 	$dumpfile("./test/uart1.vcd");
@@ -28,6 +43,7 @@ initial begin
 	$display("nsPs %d", nanosecondsPerSec);
 	$display("delay_time_f %f", delay_time_f);
 	$display("delay_time %d", delay_time);
+	$display("clocks per bit %d", bit_duration_ctr);
 
 	if (testfile == 0) begin
 		$display("Error when loading file.");
@@ -38,7 +54,8 @@ initial begin
 
 
 	// Default value for uart tx is X
-	uart_tx_test <= 1'dx;
+	//uart_tx_test <= 1'dx;
+	uart_tx_test <= 1'd1;
 	#(2ns * delay_time);
 
 	// Send each command
@@ -65,7 +82,8 @@ initial begin
 			#(2ns * delay_time);
 
 			// set tx to X to make uart frame bounds easily visible in gtkwave
-			uart_tx_test <= 1'dx;
+			//uart_tx_test <= 1'dx;
+			uart_tx_test <= 1'd1;
 			#(3ns * delay_time);
 
 			$display("uart data sent");
@@ -73,6 +91,24 @@ initial begin
 	end
 
 	$fclose(testfile);
+
+	#(10ns * delay_time);
+
+	$finish;
 end
+
+
+initial begin
+	clk <= 0;
+end
+
+initial begin
+	rst <= 1;
+
+	#10ns
+	rst <= 0;
+end
+
+always #5 clk <= ~clk;
 
 endmodule
